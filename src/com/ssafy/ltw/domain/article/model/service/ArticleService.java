016b3d207cf1483945db475878351a3d86a0e2cc
KCH <doolysmile1@naver.com>
2023-03-24 13:26:30 +0900

    feat : Article 서비스 계층 interface 작성

--- src/com/ssafy/ltw/domain/article/model/service/ArticleService.java
+++ src/com/ssafy/ltw/domain/article/model/service/ArticleService.java
@@ -1,5 +1,20 @@
 package com.ssafy.ltw.domain.article.model.service;
 
+import com.ssafy.ltw.domain.article.ArticleDto;
+import com.ssafy.ltw.global.util.PageNavigation;
+
+import java.util.List;
+import java.util.Map;
+
 public interface ArticleService {
+
+    void writeArticle(ArticleDto articleDto) throws Exception;
+    List<ArticleDto> listArticle(Map<String, String> map) throws Exception;
+    PageNavigation makePageNavigation(Map<String, String> map) throws Exception;
+    ArticleDto getArticle(int id) throws Exception;
+    void updateHit(int id) throws Exception;
+
+    void modifyArticle(ArticleDto articleDto) throws Exception;
+    void deleteArticle(int id) throws Exception;
 
 }
--- src/com/ssafy/ltw/domain/article/model/service/ArticleServiceImpl.java
+++ src/com/ssafy/ltw/domain/article/model/service/ArticleServiceImpl.java
@@ -1,5 +1,54 @@
 package com.ssafy.ltw.domain.article.model.service;
 
+import com.ssafy.ltw.domain.article.ArticleDto;
+import com.ssafy.ltw.domain.article.model.dao.ArticleDao;
+import com.ssafy.ltw.domain.article.model.dao.ArticleDaoImpl;
+import com.ssafy.ltw.global.util.PageNavigation;
+
+import java.util.List;
+import java.util.Map;
+
 public class ArticleServiceImpl implements ArticleService {
 
+    private static ArticleService articleService = new ArticleServiceImpl();
+
+    private ArticleDao articleDao;
+
+    private ArticleServiceImpl() {
+        articleDao = ArticleDaoImpl.getArticleDao();
+    }
+    @Override
+    public void writeArticle(ArticleDto articleDto) throws Exception {
+
+    }
+
+    @Override
+    public List<ArticleDto> listArticle(Map<String, String> map) throws Exception {
+        return null;
+    }
+
+    @Override
+    public PageNavigation makePageNavigation(Map<String, String> map) throws Exception {
+        return null;
+    }
+
+    @Override
+    public ArticleDto getArticle(int id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void updateHit(int id) throws Exception {
+
+    }
+
+    @Override
+    public void modifyArticle(ArticleDto articleDto) throws Exception {
+
+    }
+
+    @Override
+    public void deleteArticle(int id) throws Exception {
+
+    }
 }
