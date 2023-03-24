bd659931663b3acca5c3e3c08f8b91da13a5db39
yoojinjangjang <yoojinjangjang@gmail.com>
2023-03-24 17:48:50 +0900

    feat : create file

---
+++ src/com/ssafy/ltw/domain/member/model/dao/MemberDaoImpl.java
@@ -0,0 +1,5 @@
+package com.ssafy.ltw.domain.member.model.dao;
+
+public class MemberDaoImpl implements MemberDao{
+
+}
---
+++ src/com/ssafy/ltw/domain/member/model/service/MemberService.java
@@ -0,0 +1,5 @@
+package com.ssafy.ltw.domain.member.model.service;
+
+public interface MemberService {
+
+}
