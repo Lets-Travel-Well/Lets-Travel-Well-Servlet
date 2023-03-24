9f519f8128ae82cebfac86e0cf88376b11467903
KCH <doolysmile1@naver.com>
2023-03-24 12:21:13 +0900

    chore : article write 구현중

--- src/com/ssafy/ltw/domain/article/model/dao/ArticleDaoImpl.java
+++ src/com/ssafy/ltw/domain/article/model/dao/ArticleDaoImpl.java
@@ -1,15 +1,45 @@
 package com.ssafy.ltw.domain.article.model.dao;
 
 import com.ssafy.ltw.domain.article.ArticleDto;
+import com.ssafy.ltw.global.util.DBUtil;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
 public class ArticleDaoImpl implements ArticleDao {
 
+    private static ArticleDao articleDao;
+
+    private DBUtil dbUtil;
+
+    private ArticleDaoImpl() {
+        dbUtil = DBUtil.getInstance();
+    }
+
+    public static ArticleDao getArticleDao() {
+        if(articleDao == null){
+            articleDao = new ArticleDaoImpl();
+        }
+        return articleDao;
+    }
     @Override
     public void writeArticle(ArticleDto articleDto) throws SQLException {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        try {
+            conn = dbUtil.getConnection();
+            StringBuilder sql = new StringBuilder();
+            sql.append("insert into board (user_id, subject, content) \n");
+            sql.append("values (?, ?, ?)");
+            pstmt = conn.prepareStatement(sql.toString());
+
+            pstmt.executeUpdate();
+        } finally {
+            dbUtil.close(pstmt, conn);
+        }
 
     }
 
