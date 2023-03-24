253508669a29ccd9538da39ddbc8af943b92038c
KCH <doolysmile1@naver.com>
2023-03-24 10:22:04 +0900

    feat : Article DAO interface 구현

--- src/com/ssafy/ltw/domain/article/model/dao/ArticleDaoImpl.java
+++ src/com/ssafy/ltw/domain/article/model/dao/ArticleDaoImpl.java
@@ -1,5 +1,45 @@
 package com.ssafy.ltw.domain.article.model.dao;
 
+import com.ssafy.ltw.domain.article.ArticleDto;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
 public class ArticleDaoImpl implements ArticleDao {
 
+    @Override
+    public void writeArticle(ArticleDto articleDto) throws SQLException {
+
+    }
+
+    @Override
+    public List<ArticleDto> listArticle(Map<String, Object> param) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public int getTotalArticleCount(Map<String, Object> param) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public ArticleDto getArticle(int id) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void updateHit(int id) throws SQLException {
+
+    }
+
+    @Override
+    public void modifyArticle(ArticleDto articleDto) throws SQLException {
+
+    }
+
+    @Override
+    public void deleteArticle(int id) throws SQLException {
+
+    }
 }
