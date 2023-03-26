03f1374a37a800a4a99af9f7a5f39a65dbb9dd7a
KCH <doolysmile1@naver.com>
2023-03-26 22:02:43 +0900

    chore : TODO

--- src/test/java/com/ssafy/ltw/domain/article/ArticleServiceTestTest.java
+++ src/test/java/com/ssafy/ltw/domain/article/ArticleServiceTestTest.java
@@ -2,16 +2,15 @@ package com.ssafy.ltw.domain.article;
 
 import com.ssafy.ltw.domain.article.model.dao.ArticleDao;
 import com.ssafy.ltw.domain.article.model.dao.ArticleDaoImpl;
-import lombok.ToString;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ArticleServiceTestTest {
+    // 레포 테스르를 하는중 수정 필요
     private final ArticleDao articleDao = ArticleDaoImpl.getArticleDao();
     
     // TODO : 유저 구현시 수정해야함
