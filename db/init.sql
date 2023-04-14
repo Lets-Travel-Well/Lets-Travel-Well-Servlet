6a37e65555f2d6cd090c58f4a5e780d474adbbe9
KCH <doolysmile1@naver.com>
2023-04-14 16:46:07 +0900

    feat : ddl 추가

--- db/init.sql
+++ db/init.sql
@@ -20,12 +20,12 @@ DROP TABLE IF EXISTS `ltw_local`.`Article` ;
 
 CREATE TABLE `ltw_local`.`Article` (
                                        `id`    BIGINT    NOT NULL AUTO_INCREMENT,
-                                       `created_date`    TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                       `modified_date`    TIMESTAMP    NULL DEFAULT NULL,
                                        `subject`    VARCHAR(100)    NULL DEFAULT NULL,
                                        `content`    VARCHAR(2000)    NULL DEFAULT NULL,
                                        `hit`    INT NULL DEFAULT 0,
                                        `member_id`    BIGINT    NOT NULL,
+                                       `created_date`    TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                       `modified_date`    TIMESTAMP    NULL DEFAULT NULL,
                                        PRIMARY KEY (`id`),
                                        FOREIGN KEY (member_id) REFERENCES Member(id)
 );
@@ -54,4 +54,56 @@ CREATE TABLE `ltw_local`.`Salt`
     `salt`      VARCHAR(2000) NULL DEFAULT NULL,
     PRIMARY KEY (`id`),
     FOREIGN KEY (member_id) REFERENCES Member (id)
+);
+
+DROP TABLE IF EXISTS `ltw_local`.`attraction_path`;
+CREATE TABLE `ltw_local`.`attraction_path` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `created_date` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+    `modified_date` TIMESTAMP NOT NULL,
+    `member_id` BIGINT NOT NULL,
+    `attraction_course` VARCHAR(2000) NOT NULL,
+    `subject` VARCHAR(100) NOT NULL,
+    `content` VARCHAR(2000) NULL,
+    `like` INT NULL DEFAULT 0,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fk_attraction_path_member1`
+    FOREIGN KEY (`member_id`)
+    REFERENCES `ltw_local`.`member` (`id`)
+);
+
+
+DROP TABLE IF EXISTS `ltw_local`.`attraction_path_like_member`;
+
+CREATE TABLE `ltw_local`.`attraction_path_like_member` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `attraction_path_id` INT NOT NULL,
+    `member_id` BIGINT NOT NULL,
+    PRIMARY KEY (`id`),
+
+    CONSTRAINT `fk_attraction_path_like_member_attraction_path1`
+    FOREIGN KEY (`attraction_path_id`)
+    REFERENCES `ltw_local`.`attraction_path` (`id`),
+    CONSTRAINT `fk_attraction_path_like_member_member1`
+    FOREIGN KEY (`member_id`)
+    REFERENCES `ltw_local`.`member` (`id`)
+);
+
+DROP TABLE IF EXISTS `ltw_local`.`comment`;
+
+CREATE TABLE `ltw_local`.`comment` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `article_id` BIGINT NOT NULL,
+    `member_id` BIGINT NOT NULL,
+    `subject` VARCHAR(100) NULL,
+    `content` VARCHAR(1000) NOT NULL,
+    `created_date` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+    `modified_date` TIMESTAMP NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fk_comment_article1`
+    FOREIGN KEY (`article_id`)
+    REFERENCES `ltw_local`.`article` (`id`),
+    CONSTRAINT `fk_comment_member1`
+    FOREIGN KEY (`member_id`)
+    REFERENCES `ltw_local`.`member` (`id`)
 );
